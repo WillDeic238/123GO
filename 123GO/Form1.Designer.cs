055dbfc1881daedcb996d56b51f84b1efb4d4d14
willdeic238 <willdeic238@SDS-L-J9HDF63>
2024-02-09 14:05:16 -0500

    Add project files.

---
+++ 123GO/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace _123GO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartGame = new System.Windows.Forms.Button();
+            this.Countdown = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // StartGame
+            // 
+            this.StartGame.Location = new System.Drawing.Point(214, 98);
+            this.StartGame.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.StartGame.Name = "StartGame";
+            this.StartGame.Size = new System.Drawing.Size(172, 58);
+            this.StartGame.TabIndex = 0;
+            this.StartGame.Text = "StartGame";
+            this.StartGame.UseVisualStyleBackColor = true;
+            this.StartGame.Click += new System.EventHandler(this.StartGame_Click);
+            // 
+            // Countdown
+            // 
+            this.Countdown.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Countdown.Location = new System.Drawing.Point(181, 193);
+            this.Countdown.Name = "Countdown";
+            this.Countdown.Size = new System.Drawing.Size(271, 60);
+            this.Countdown.TabIndex = 1;
+            this.Countdown.Text = " ";
+            this.Countdown.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(659, 335);
+            this.Controls.Add(this.Countdown);
+            this.Controls.Add(this.StartGame);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "Form1";
+            this.Text = "321Go";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartGame;
+        private System.Windows.Forms.Label Countdown;
+    }
+}
+
---
+++ 123GO/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace _123GO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StartGame_Click(object sender, EventArgs e)
+        { //change the color of the button
+            StartGame.Visible = false;
+            //change the message in the output label
+            Countdown.ForeColor = Color.White;
+            Countdown.Text = "Game Starts In . . . 3";
+            SoundPlayer player = new SoundPlayer(Properties.Resources.ShortDing);
+            player.Play();    
+            Refresh();
+            Thread.Sleep(1000);
+            
+            Countdown.Text = "Game Starts In . . . 2";
+
+            player.Play();
+
+            Refresh();
+            Thread.Sleep(1000);
+
+
+            Countdown.Text = "Game Starts In . . . 1";
+
+            player.Play();
+
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            Countdown.Text = "GO!!!!!";
+            SoundPlayer player3 = new SoundPlayer(Properties.Resources.LongDing);
+            player3.Play();
+            this.BackColor = Color.Green;
+
+   
+
+        }
+
+        private void Countdown_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
